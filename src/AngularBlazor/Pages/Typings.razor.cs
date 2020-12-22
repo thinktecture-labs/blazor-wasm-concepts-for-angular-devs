bb418c7febff70155db74726092d0ada9dae9bf5
Steffen Jahr <steffen.jahr@thinktecture.com>
2020-12-22 16:54:23 +0100

    feat: add typings page

---
+++ src/AngularBlazor/Pages/Typings.razor.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace AngularBlazor.Pages
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public partial class Typings
+    {
+        private readonly Person _examplePerson;
+        private string _fieldName = "FirstName";
+        private string _fieldValue;
+        
+        public Typings()
+        {
+            var firstPerson = new Person
+            {
+                FirstName = "Max",
+                LastName = "Mustermann"
+            };
+
+            Console.WriteLine($"Hello {firstPerson.FirstName} {firstPerson.LastName}");
+
+            dynamic secondPerson = new {FirstName = "Luke", LastName = "Skywalker"};
+            Console.WriteLine($"Hello {secondPerson.FirstName} {secondPerson.LastName}");
+
+            // NOT WORKING
+            // secondPerson["FirstName"] = "Han";
+            // secondPerson["LastName"] = "Solo";
+
+            // Console.WriteLine($"Hello {secondPerson.FirstName} {secondPerson.LastName}");
+            
+            // Only works for objects that are typed. NOT for dynamics
+            firstPerson.GetType().GetProperty("FirstName")?.SetValue(firstPerson, "Han");
+            firstPerson.GetType().GetProperty("LastName")?.SetValue(firstPerson, "Solo");
+            
+            Console.WriteLine($"Hello {firstPerson.FirstName} {firstPerson.LastName}");
+
+            _examplePerson = new Person
+            {
+                FirstName = "Lando",
+                LastName = "Calrissian"
+            };
+        }
+
+        private void ChangeFieldValue()
+        {
+            var propertyInfo =_examplePerson.GetType().GetProperty(this._fieldName);
+            if (propertyInfo != null)
+            {
+                propertyInfo.SetValue(_examplePerson, _fieldValue);
+            }
+            Console.WriteLine($"Value of {_fieldName} changed to {_fieldValue}");
+            StateHasChanged();
+        }
+    }
+}
