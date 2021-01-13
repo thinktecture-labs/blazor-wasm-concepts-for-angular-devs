6f6d5bee2be8b2a2b0fad5ef4b6bc7be2a6c5f16
Steffen Jahr <steffen.jahr@thinktecture.com>
2021-01-13 12:21:19 +0100

    chore: style updates

--- src/AngularBlazor/Shared/MainLayout.razor.cs
+++ src/AngularBlazor/Shared/MainLayout.razor.cs
@@ -1,8 +1,19 @@
+using MudBlazor;
+
 namespace AngularBlazor.Shared
 {
     public partial class MainLayout
     {
         private bool _drawerOpen = true;
+
+        private readonly MudTheme _customTheme = new()
+        {
+            Palette = new Palette()
+            {
+                Primary = "#5C2D91",
+                AppbarBackground = "#5C2D91"
+            }
+        };
 
         private void DrawerToggle()
         {
