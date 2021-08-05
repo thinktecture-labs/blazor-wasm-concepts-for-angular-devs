85082144275f1b5a2d37fbe4349415102bea096c
Steffen Jahr <steffen.jahr@thinktecture.com>
2021-08-05 21:27:34 +0200

    chore: update typings sample

--- src/AngularBlazor/Pages/Typings.razor.cs
+++ src/AngularBlazor/Pages/Typings.razor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 
 namespace AngularBlazor.Pages
@@ -13,6 +14,7 @@ namespace AngularBlazor.Pages
     public partial class Typings
     {
         private readonly Person _examplePerson;
+        private readonly dynamic _expandoPerson;
         private string _fieldName = "FirstName";
         private string _fieldValue;
         private IEnumerable<string> _fieldnames;
@@ -26,16 +28,22 @@ namespace AngularBlazor.Pages
             };
 
             _fieldnames = _examplePerson.GetType().GetProperties().ToList().Select(p => p.Name);
+
+            _expandoPerson = new ExpandoObject();
+            _expandoPerson.FirstName = "Luke";
+            _expandoPerson.LastName = "Skywalker";
         }
 
         private void ChangeFieldValue()
         {
             
-            var propertyInfo = _examplePerson.GetType().GetProperty(this._fieldName);
+            var propertyInfo = _examplePerson.GetType().GetProperty(_fieldName);
             if (propertyInfo != null)
             {
                 propertyInfo.SetValue(_examplePerson, _fieldValue);
             }
+
+            ((IDictionary<string,object>)_expandoPerson)[_fieldName] = _fieldValue;
 
             Console.WriteLine($"Value of {_fieldName} changed to {_fieldValue}");
         }
